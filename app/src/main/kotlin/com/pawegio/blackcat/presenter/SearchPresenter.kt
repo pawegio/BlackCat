fda1f2ed8e014fd3032d6f5426d60bd6cd9e9383
Paweł Gajda <pawegio@gmail.com>
2016-08-22 19:11:08 +0200

    Implemented unit tests passing presenters logic

--- app/src/main/kotlin/com/pawegio/blackcat/presenter/SearchPresenter.kt
+++ app/src/main/kotlin/com/pawegio/blackcat/presenter/SearchPresenter.kt
@@ -3,15 +3,30 @@ package com.pawegio.blackcat.presenter
 import com.pawegio.blackcat.contract.SearchContract
 import com.pawegio.blackcat.domain.Repository
 import com.pawegio.blackcat.domain.UserResult
+import com.pawegio.kandroid.e
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
 
 class SearchPresenter(private val view: SearchContract.View,
                       private val repository: Repository) : SearchContract.Presenter {
 
     override fun searchResults(query: String) {
-
+        view.showProgressBar()
+        repository.getSearchResults(query)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        { results ->
+                            view.hideProgressBar()
+                            view.showResults(results)
+                        },
+                        { error ->
+                            e(error.message.toString())
+                        }
+                )
     }
 
     override fun openUserDetails(user: UserResult) {
-
+        view.showUserDetails(user.login)
     }
 }
--- app/src/main/kotlin/com/pawegio/blackcat/presenter/UserDetailsPresenter.kt
+++ app/src/main/kotlin/com/pawegio/blackcat/presenter/UserDetailsPresenter.kt
@@ -2,11 +2,24 @@ package com.pawegio.blackcat.presenter
 
 import com.pawegio.blackcat.contract.UserDetailsContract
 import com.pawegio.blackcat.domain.Repository
+import com.pawegio.kandroid.e
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
 
 class UserDetailsPresenter(private val view: UserDetailsContract.View,
                            private val repository: Repository) : UserDetailsContract.Presenter {
 
     override fun loadUserDetails(username: String) {
-
+        repository.getUserDetails(username)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        { userDetails ->
+                            view.updateUserDetails(userDetails)
+                        },
+                        { error ->
+                            e(error.message.toString())
+                        }
+                )
     }
 }
