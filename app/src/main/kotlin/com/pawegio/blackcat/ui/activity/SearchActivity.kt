b03f9b5d48201d1486c179f582e628c71f904a24
Paweł Gajda <pawegio@gmail.com>
2016-08-23 00:09:03 +0200

    Implemented search results adapter

--- app/src/main/kotlin/com/pawegio/blackcat/ui/activity/SearchActivity.kt
+++ app/src/main/kotlin/com/pawegio/blackcat/ui/activity/SearchActivity.kt
@@ -1,4 +1,4 @@
-package com.pawegio.blackcat.ui
+package com.pawegio.blackcat.ui.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
---
+++ app/src/main/kotlin/com/pawegio/blackcat/ui/adapter/ResultsAdapter.kt
@@ -0,0 +1,55 @@
+package com.pawegio.blackcat.ui.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import com.pawegio.blackcat.R
+import com.pawegio.blackcat.domain.RepositoryResult
+import com.pawegio.blackcat.domain.SearchResult
+import com.pawegio.blackcat.domain.UserResult
+import com.pawegio.kandroid.inflateLayout
+import kotlinx.android.synthetic.main.item_result.view.*
+
+/**
+ * @author pawegio
+ */
+class ResultsAdapter(val onClick: (SearchResult) -> Unit) : RecyclerView.Adapter<ResultsAdapter.ViewHolder>() {
+
+    private val data = mutableListOf<SearchResult>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(parent.context.inflateLayout(R.layout.item_result, parent, false), onClick)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+        holder?.bindItem(data[position])
+    }
+
+    override fun getItemCount() = data.size
+
+    fun updateData(results: List<SearchResult>) {
+        data.clear()
+        data.addAll(results)
+        notifyDataSetChanged()
+    }
+
+    class ViewHolder(val view: View, val onClick: (SearchResult) -> Unit) : RecyclerView.ViewHolder(view) {
+
+        fun bindItem(item: SearchResult) {
+            with(itemView) {
+                when(item) {
+                    is UserResult -> {
+                        titleView.text = item.login
+                        typeView.text = "User"
+                        setOnClickListener { onClick(item) }
+                    }
+                    is RepositoryResult -> {
+                        titleView.text = item.name
+                        typeView.text = "Repository"
+                        setOnClickListener(null)
+                    }
+                }
+            }
+        }
+    }
+}
